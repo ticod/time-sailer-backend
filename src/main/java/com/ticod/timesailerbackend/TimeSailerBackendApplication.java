9d733f03184d2b38fbe4ca00d36c1c2c7852baa1
tico <lours2021@gmail.com>
2024-07-08 22:15:35 +0900

    Initial commit

---
+++ src/main/java/com/ticod/timesailerbackend/TimeSailerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ticod.timesailerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSailerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeSailerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticod/timesailerbackend/TimeSailerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticod.timesailerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSailerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
