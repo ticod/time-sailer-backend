7fe5540d0df6a7c3326565d812b0ea59ca506cbb
tico <lours2021@gmail.com>
2024-07-09 19:28:01 +0900

    feat Update ErrorCode
    ErrorCode 추가 및 수정
    
    USER_DUPLICATED
    -> USER_EMAIL_DUPLICATED
    조금 더 직관적인 이름으로 수정
    
    USER_EMAIL_NOT_FOUND,
    INVALID_PASSWORD,
    ACCESS_TOKEN_EXPIRED
    로그인 관련 에러 코드 추가

--- src/main/java/com/ticod/timesailerbackend/exception/ErrorCode.java
+++ src/main/java/com/ticod/timesailerbackend/exception/ErrorCode.java
@@ -8,7 +8,13 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    USER_DUPLICATED(HttpStatus.CONFLICT, "이미 가입된 회원");
+    // 401 Error
+    USER_EMAIL_NOT_FOUND(HttpStatus.UNAUTHORIZED, "가입되지 않은 회원"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호"),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 엑세스 토큰"),
+    // 409 Error
+    USER_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "이미 가입된 회원"),
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
