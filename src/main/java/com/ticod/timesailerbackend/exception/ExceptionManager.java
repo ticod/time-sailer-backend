b2e1a46a7147ee6fc0cc95b63711321c7781fd88
tico <lours2021@gmail.com>
2024-07-09 19:29:27 +0900

    feat Update ExceptionManager
    AppException 추가에 따른 Handler 추가
    ResponseEntity.body()에 담을
    ResponseBody 내장 클래스 추가

--- src/main/java/com/ticod/timesailerbackend/exception/ExceptionManager.java
+++ src/main/java/com/ticod/timesailerbackend/exception/ExceptionManager.java
@@ -1,5 +1,7 @@
 package com.ticod.timesailerbackend.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +12,23 @@ public class ExceptionManager {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<?> appExceptionHandler(AppException e) {
+
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+                .body(new ResponseBody(e.getErrorCode().name(), e.getMessage()));
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private static class ResponseBody {
+        private String errorName;
+        private String errorMessage;
     }
 
 }
