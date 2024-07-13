3ad91e38a5ebd10e7ad985ffca7b20c6384bf504
tico <lours2021@gmail.com>
2024-07-14 05:15:50 +0900

    refactor Update JwtAuthenticationFilter
    doFilterInternal 메서드 매개변수에
    @NonNull 추가, 경고선 제거

--- src/main/java/com/ticod/timesailerbackend/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/ticod/timesailerbackend/filter/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -31,8 +32,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         // Header 꺼내기
