193fe8bfa6c1daea7d98139b6c9935cce6f9e48a
tico <lours2021@gmail.com>
2024-07-14 05:14:45 +0900

    feat Add SwaggerConfig
    Swagger 설정에 리프레쉬 토큰, 엑세스 토큰 추가

---
+++ src/main/java/com/ticod/timesailerbackend/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.ticod.timesailerbackend.config;
+
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OperationCustomizer globalHeader() {
+        return ((operation, handlerMethod) -> {
+            operation.addParametersItem(new Parameter()
+                            .in(ParameterIn.HEADER.toString())
+                            .schema(new StringSchema().name("refreshToken"))
+                            .name("refreshToken"));
+            return operation;
+        });
+    }
+
+    @Bean
+    public OpenAPI api() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .bearerFormat("JWT")
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement);
+    }
+
+}
