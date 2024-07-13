73135e67559122f3d42da23189ce619c24c3d2c9
tico <lours2021@gmail.com>
2024-07-14 02:46:11 +0900

    feat Update User
    UserDetails 제거
    refresh token 갱신 메서드 추가
    password encoding 메서드 추가

--- src/main/java/com/ticod/timesailerbackend/entity/User.java
+++ src/main/java/com/ticod/timesailerbackend/entity/User.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,7 +19,7 @@ import java.util.Collection;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -33,48 +34,17 @@ public class User implements UserDetails {
     @Column(name = "password", nullable = false, length = 100)
     private String password;
 
-    @Column(name = "password_salt", nullable = false, length = 100)
-    private String passwordSalt;
-
-    @Column(name = "refresh_token", nullable = false, length = 100)
+    @Column(name = "refresh_token", nullable = false, length = 200)
     private String refreshToken;
 
     @Column(name = "created_at", nullable = false)
     private LocalDate createdAt;
 
-    // UserDetails methods
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<>();
+    public void updateRefreshToken(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
     }
 
-    @Override
-    public String getUsername() {
-        return this.name;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
+    public void encodePasswordBy(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(password);
     }
 }
