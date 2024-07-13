02d697d73cf1e6e0bcaa38ef6848c86fffba2891
tico <lours2021@gmail.com>
2024-07-14 05:09:58 +0900

    feat Add SecurityConfig
    JwtAuthenticationFilter 추가에 따른
    Spring security 설정 추가
    session 방식 비허용 및 필터 추가,
    swagger url 허용 및 회원가입 url 허용

---
+++ src/main/java/com/ticod/timesailerbackend/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.ticod.timesailerbackend.config;
+
+import com.ticod.timesailerbackend.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests
+                            // Swagger
+                            .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
+                            // 회원가입
+                            .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+
+
+}
